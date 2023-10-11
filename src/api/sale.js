87d16d8b2691b589c8736956ed889bbf65190f4b
Grigoriy Kovalenko <g.kovalenko@pikabu.ru>
2023-10-11 19:47:44 +0300

    fix(env): add base url for images

--- src/api/sale.js
+++ src/api/sale.js
@@ -1,30 +1,32 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 const saleItems = [
   {
     id: 1,
     name: 'Свитер унисекс',
     price: 3_100,
-    image: '/images/sale_items/s4-1.png',
+    image: BASE_URL + 'images/sale_items/s4-1.png',
     sale: 30,
   },
   {
     id: 2,
     name: 'Свитер женский',
     price: 4_200,
-    image: '/images/sale_items/s4-2.png',
+    image: BASE_URL + 'images/sale_items/s4-2.png',
     sale: 30,
   },
   {
     id: 3,
     name: 'Пальто',
     price: 3_100,
-    image: '/images/sale_items/s4-3.png',
+    image: BASE_URL + 'images/sale_items/s4-3.png',
     sale: 30,
   },
   {
     id: 4,
     name: 'Футболка',
     price: 2_400,
-    image: '/images/sale_items/s4-4.png',
+    image: BASE_URL + 'images/sale_items/s4-4.png',
     sale: 30,
   },
 ];
