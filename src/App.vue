3ae0dd909e9a4e88bd07b70a4abc08b8c5c962fc
Lolinelyf <lolinelyf@mail.ru>
2023-10-16 11:38:50 +0300

    qwe

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@ import FooterView from './views/FooterView.vue';
 import ToastsView from './views/ToastsView.vue';
 import BrandView from './views/BrandView.vue';
 import CategoriesView from './views/CategoriesView.vue';
-import NavbarView from './views/NavbarView.vue';
+// import NavbarView from './views/NavbarView.vue';
 import SaleView from './views/SaleView.vue';
 </script>
 
 <template>
   <HeaderView />
   <main>
-    <NavbarView />
+    <!-- <NavbarView /> -->
     <BrandView />
     <CategoriesView />
     <SaleView />
