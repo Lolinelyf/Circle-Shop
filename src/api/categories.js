e6b7a4afa4c573e1eefff2a4d17b061e3ae8f202
Grigoriy Kovalenko <g.kovalenko@pikabu.ru>
2023-10-17 13:41:29 +0300

    fix paths

--- src/api/categories.js
+++ src/api/categories.js
@@ -2,57 +2,57 @@ const categoriesItems = [
   {
     id: 1,
     name: 'Верхняя одежда',
-    image: 'images/categories/s3-1.png',
+    image: '/images/categories/s3-1.png',
   },
   {
     id: 2,
     name: 'Рубашки',
-    image: 'images/categories/s3-2.png',
+    image: '/images/categories/s3-2.png',
   },
   {
     id: 3,
     name: 'Платья',
-    image: 'images/categories/s3-3.png',
+    image: '/images/categories/s3-3.png',
   },
   {
     id: 4,
     name: 'Свитеры и толстовки',
-    image: 'images/categories/s3-4.png',
+    image: '/images/categories/s3-4.png',
   },
   {
     id: 5,
     name: 'Сумки',
-    image: 'images/categories/s3-5.png',
+    image: '/images/categories/s3-5.png',
   },
   {
     id: 6,
     name: 'Обувь',
-    image: 'images/categories/s3-6.png',
+    image: '/images/categories/s3-6.png',
   },
   {
     id: 7,
     name: 'Рубашки',
-    image: 'images/categories/s3-2.png',
+    image: '/images/categories/s3-2.png',
   },
   {
     id: 8,
     name: 'Платья',
-    image: 'images/categories/s3-3.png',
+    image: '/images/categories/s3-3.png',
   },
   {
     id: 9,
     name: 'Свитеры и толстовки',
-    image: 'images/categories/s3-4.png',
+    image: '/images/categories/s3-4.png',
   },
   {
     id: 10,
     name: 'Сумки',
-    image: 'images/categories/s3-5.png',
+    image: '/images/categories/s3-5.png',
   },
   {
     id: 11,
     name: 'Обувь',
-    image: 'images/categories/s3-6.png',
+    image: '/images/categories/s3-6.png',
   },
 ];
 
